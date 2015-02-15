2c75b958fa00e1dde5fe68260ffa5d7320e11254
David Pritchard <daveagp@gmail.com>
2015-02-14 21:08:17 -0500

    try larger amount of memory as suggested by Philip Guo

--- cp/traceprinter/InMemory.java
+++ cp/traceprinter/InMemory.java
@@ -181,7 +181,7 @@ http://docs.oracle.com/javase/7/docs/jdk/api/jpda/jdi/com/sun/jdi/connect/Connec
 
             // set a memory limit
             
-            options += "-Xmx128M" + " ";
+            options += "-Xmx512M" + " ";
             
             options += "-Dfile.encoding=UTF-8" + " ";
 
